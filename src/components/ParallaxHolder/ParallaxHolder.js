b80d79daa14d6ebc906218ce688b94c41e8222c1
Vedran Aurer <vaurer@gmail.com>
2020-12-29 09:58:07 +0100

    alt key

--- src/components/ParallaxHolder/ParallaxHolder.js
+++ src/components/ParallaxHolder/ParallaxHolder.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons.cjs';
-import styles from './ParallaxHolder.module.css';
+// import styles from './ParallaxHolder.module.css';
 
 export default class ParallaxHolder extends Component {
     render(){
@@ -10,7 +10,7 @@ export default class ParallaxHolder extends Component {
         // const Lightblue = ({ children }) => <span style={{ color: '#9AEDFE' }}>{children}</span>
         // const Green = ({ children }) => <span style={{ color: '#57EE89' }}>{children}</span>
         // const Blue = ({ children }) => <span style={{ color: '#57C7FF' }}>{children}</span> 
-         const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>
+        //  const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>
         return(
             <div>
                <Parallax ref={(ref) => (this.parallax = ref)} pages={3} style={{ backgroundColor: 'Gray' }}>
@@ -20,37 +20,37 @@ export default class ParallaxHolder extends Component {
         <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} />
 
         <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+          <img src={url('satellite4')} alt={'1'} style={{ width: '15%', marginLeft: '70%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+          <img src={url('cloud')} alt={'1'} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={2.5} speed={-0.4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
-          <img src={url('earth')} style={{ width: '60%' }} />
+          <img src={url('earth')} alt={'1'} style={{ width: '60%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -67,7 +67,7 @@ export default class ParallaxHolder extends Component {
           speed={0.1}
           onClick={() => this.parallax.scrollTo(1)}
           style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <img src={this.props.src} style={{ width: 'auto', opacity: 0.7 }} />
+          <img src={this.props.src} alt={'1'} style={{ width: 'auto', opacity: 0.7 }} />
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -75,7 +75,7 @@ export default class ParallaxHolder extends Component {
           speed={0.1}
           onClick={() => this.parallax.scrollTo(2)}
           style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <img src={this.props.src} style={{ width: '40%' }} />
+          <img src={this.props.src} alt={'1'} style={{ width: '40%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -83,7 +83,7 @@ export default class ParallaxHolder extends Component {
           speed={-0}
           style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
           onClick={() => this.parallax.scrollTo(0)}>
-          <img src={this.props.src} style={{ width: '40%' }} /> 
+          <img src={this.props.src} alt={'1'} style={{ width: '40%' }} /> 
         </ParallaxLayer>
       </Parallax>
             </div>
