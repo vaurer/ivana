893873bc0e78b91722c11bbc67efe90f92070a30
Vedran Aurer <vaurer@gmail.com>
2021-01-11 20:21:37 +0100

    added netlify form with corredt submit

--- src/components/Footer/Form.js
+++ src/components/Footer/Form.js
@@ -1,58 +1,78 @@
-import React, { Component } from 'react';
-import styles from './Form.module.css';
+import React, { Component } from "react";
+import styles from "./Form.module.css";
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            firstName: '',
-            lastName: '',
-            userEmail: '',
-            message: '',
-            userTelefonNummber: ''
-         }
-    }
-    changeHandler = (e) =>{
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    onSubmit = (e) =>{
-     
-        this.props.onSubmit(this.state);
-        this.setState({
-            firstName: '',
-            lastName: '',
-            userEmail: '',
-            message: '',
-            userTelefonNummber: ''
-        })
-       
-    }
-    render() { 
-        return ( 
-            <div className={styles.box}>
-                <h1>Wir sind für dich da</h1>
-            <form actíon="POST" data-netlify="true">
-              <input name="firstName" value={this.state.firstName} onChange={e => this.changeHandler(e)}></input>
-              <label>Vorname: </label>
-              <br/>
-              <input name="lastName"  value={this.state.lastName} onChange={e => this.changeHandler(e)}></input>
-              <label>Nachname: </label>
-              <br/>
-              <input name="userEmail" value={this.state.userEmail} onChange={e => this.changeHandler(e)}></input>
-              <label>E-mail: </label>
-              <br/>
-              <textarea name="message" value={this.state.message} onChange={e => this.changeHandler(e)}></textarea>
-              <label>Nachricht: </label>
-              <br/>
-              <input name="userTelefonNummber" value={this.state.userTelefonNummber} onChange={e => this.changeHandler(e)}></input>
-              <label>Telefonnummer: </label>
-              <br/><input type="submit" value="absenden"></input>
-              <button onClick={e => this.onSubmit(e)}>Absenden</button>  
-            </form>
-            </div>
-         );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      userEmail: "",
+      message: "",
+      userTelefonNummber: "",
+    };
+  }
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  onSubmit = (e) => {
+    this.props.onSubmit(this.state);
+    this.setState({
+      firstName: "",
+      lastName: "",
+      userEmail: "",
+      message: "",
+      userTelefonNummber: "",
+    });
+  };
+  render() {
+    return (
+      <div className={styles.box}>
+        <h1>Wir sind für dich da</h1>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <input
+            name="firstName"
+            value={this.state.firstName}
+            onChange={(e) => this.changeHandler(e)}
+          ></input>
+          <label>Vorname: </label>
+          <br />
+          <input
+            name="lastName"
+            value={this.state.lastName}
+            onChange={(e) => this.changeHandler(e)}
+          ></input>
+          <label>Nachname: </label>
+          <br />
+          <input
+            name="userEmail"
+            value={this.state.userEmail}
+            onChange={(e) => this.changeHandler(e)}
+          ></input>
+          <label>E-mail: </label>
+          <br />
+          <textarea
+            name="message"
+            value={this.state.message}
+            onChange={(e) => this.changeHandler(e)}
+          ></textarea>
+          <label>Nachricht: </label>
+          <br />
+          <input
+            name="userTelefonNummber"
+            value={this.state.userTelefonNummber}
+            onChange={(e) => this.changeHandler(e)}
+          ></input>
+          <label>Telefonnummer: </label>
+          <br />
+          <input type="submit" value="absenden"></input>
+         
+        </form>
+      </div>
+    );
+  }
 }
 export default Form;
