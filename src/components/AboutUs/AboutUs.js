dcab5cedabe41f74304193da4a0f60a326b99abd
Vedran Aurer <vaurer@gmail.com>
2021-01-16 12:17:20 +0100

    about us

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,26 +1,53 @@
-// import React, { Component } from "react";
-// import styles from "./AboutUs.module.css";
+import React, { Component } from "react";
+import styles from "./AboutUs.module.css";
 
-// class AboutUs extends Component {
-//   state = {};
-//   render() {
-//     return (
-//         <div>
-      
-//       <div className={styles.row}>
-//   <div className={styles.column}>
-//     <img src="img_snow.jpg" alt="Snow" style={{width:'100%'}}>
-//   </div>
-//   <div class="column">
-//     <img src="img_forest.jpg" alt="Forest" style={{width:'100%'}}>
-//   </div>
-//   <div class="column">
-//     <img src="img_mountains.jpg" alt="Mountains" style={{width:'100%'}}>
-//   </div>
-// </div>
+class AboutUs extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isLoaded: false,
+          aboutUsArray: [],
+        };
+      }
 
-//         </div>
-//     );
-//   }
-// }
-// export default AboutUs;
+  componentDidMount() {
+    fetch(Constants.aboutURL)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            aboutUsArray: result.data,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  getAboutUsPictures = () => {
+    let widgets = [];
+    this.state.aboutUsArray.forEach((element) => {
+      widgets.push(
+      <div className={styles.column}>
+        <img src={element.image.data.thumbnails[3].url} alt={element.image.title} style={{width:'100%'}}/>
+    </div>);
+    });
+    return widgets;
+  };
+
+  render() {
+    return (
+        <div>
+            <div className={styles.row}>
+            <div>{this.getAboutUsPictures()}</div>
+            </div>
+        </div>
+    );
+  }
+}
+export default AboutUs;
