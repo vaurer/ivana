ce6674f484f4794bd192992abf58ca25822d0efe
Vedran Aurer <vaurer@gmail.com>
2021-01-16 11:27:01 +0100

    AboutUs

---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,26 @@
+// import React, { Component } from "react";
+// import styles from "./AboutUs.module.css";
+
+// class AboutUs extends Component {
+//   state = {};
+//   render() {
+//     return (
+//         <div>
+      
+//       <div className={styles.row}>
+//   <div className={styles.column}>
+//     <img src="img_snow.jpg" alt="Snow" style={{width:'100%'}}>
+//   </div>
+//   <div class="column">
+//     <img src="img_forest.jpg" alt="Forest" style={{width:'100%'}}>
+//   </div>
+//   <div class="column">
+//     <img src="img_mountains.jpg" alt="Mountains" style={{width:'100%'}}>
+//   </div>
+// </div>
+
+//         </div>
+//     );
+//   }
+// }
+// export default AboutUs;
