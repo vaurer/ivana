c2dfd06d9209dc53249b614a026f2515923b4b34
HBeate <hauser_b@hotmail.com>
2021-02-06 12:56:17 +0100

    format impressum

--- src/components/Impressum/Impressum.js
+++ src/components/Impressum/Impressum.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import styles from './ImpressumHolder.module.css';
+
 class Impressum extends Component {
     state= {}
     render() {
         return (
             <div>
                 <div>
-                    <div>{this.props.name}</div><br></br>
+                    <div className={styles.bold}>{this.props.name}</div>
                     <div>{this.props.street}</div>
-                    <div>{this.props.zipcity}</div>
+                    <div>{this.props.zipcity}</div><br></br>
                     <div>{this.props.phone}</div>
                     <div>{this.props.email}</div><br></br>
                     <div>{this.props.uid}</div>
